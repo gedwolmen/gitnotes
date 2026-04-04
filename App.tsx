df4a000937e4edd800e2d025f2ef8ed63e34b7c0
Vidwa De Seram <vidwanirmaldeseram@gmail.com>
2026-04-05 02:33:50 +0530

    Add app entry point
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+import { NoteProvider } from './src/contexts/NoteContext';
+import { ThemeProvider } from './src/contexts/ThemeContext';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <NoteProvider>
+        <StatusBar style="auto" />
+        <AppNavigator />
+      </NoteProvider>
+    </ThemeProvider>
+  );
+}
