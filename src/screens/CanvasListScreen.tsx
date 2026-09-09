552083898dd5729078cccb3352e00958c068f047
Vidwa De Seram <vidwanirmaldeseram@gmail.com>
2026-09-09 20:03:06 +0530

    fix: remove fullWidth from template modal; add back button to canvases list (#1497)

--- src/screens/CanvasListScreen.tsx
+++ src/screens/CanvasListScreen.tsx
@@ -359,6 +359,7 @@ export default function CanvasListScreen() {
       </Modal>
       <ScreenHeader
         title={t('canvases.title')}
+        onBack={() => navigation.goBack()}
         actions={
           <>
             <IconButton
