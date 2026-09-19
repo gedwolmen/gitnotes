f2c487a9491af01991c8a6323929edbffbff8a40
Vidwa De Seram <vidwanirmaldeseram@gmail.com>
2026-09-19 13:49:06 +0530

    fix(list): add elevation:2 for Android selection indicator
    Lower elevation to 2 to reduce Android shadow rectangle artifact
    while maintaining visible selection indicator on Android.

--- src/components/list/SwipeableListItem.tsx
+++ src/components/list/SwipeableListItem.tsx
@@ -83,6 +83,7 @@ export function SwipeableListItem({
           shadowOpacity: 0.55,
           shadowRadius: 10,
           shadowOffset: { width: 0, height: 0 },
+          elevation: 2,
         },
         animatedStyle,
       ]}
